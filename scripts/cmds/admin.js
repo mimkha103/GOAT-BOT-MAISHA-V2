1558ce7ac3e516a71d7e2bbd94d461d960cd00a3
mimkha103 <batakuttara@gmail.com>
2025-10-27 11:47:28 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -1,4 +1,4 @@
-/cmd install owner.js const axios = require("axios");
+const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
 const moment = require("moment-timezone");
